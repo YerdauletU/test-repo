6d044ba6cd758cb67881f7e29fe51d20e2863a25
Утарбаев Ердаулет <@itstep.loc>
2022-10-18 21:13:43 +0600

    task with folders done

---
+++ script.js
@@ -0,0 +1,43 @@
+function show_parent_folder_name(subfolder_name) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let item of arr[i].folders) {
+            if (subfolder_name === item.name) {
+                console.log(arr[i].name);
+            }
+        }
+    }
+}
+
+let arr = [
+    {
+        name: "Folder 1",
+        folders: [
+            {name: "Subfolder 1"},
+            {name: "Subfolder 2"},
+            {name: "Subfolder 3"},
+        ]
+    },
+
+    {
+        name: "Folder 2",
+        folders: [
+            {name: "Subfolder 4"},
+            {name: "Subfolder 5"},
+            {name: "Subfolder 6"},
+        ]
+    },
+
+    {
+        name: "Folder 3",
+        folders: [
+            {name: "Subfolder 7"},
+            {name: "Subfolder 8"},
+            {name: "Subfolder 9"},
+        ]
+    },
+];
+
+show_parent_folder_name('Subfolder 4');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
